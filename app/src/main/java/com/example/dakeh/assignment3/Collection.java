dd8b3ca691d54a6ff0326dd83833d5a8048a4fca
SkyWasabi <dakeheng@hotmail.com>
2016-05-06 01:15:48 +1000

    get collection complete

--- app/src/main/java/com/example/dakeh/assignment3/Collection.java
+++ app/src/main/java/com/example/dakeh/assignment3/Collection.java
@@ -4,6 +4,14 @@ package com.example.dakeh.assignment3;
  * Created by dakeh on 5/2/2016.
  */
 public class Collection {
+    int id;
     String name;
 
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/example/dakeh/assignment3/ScrapbookModel.java
+++ app/src/main/java/com/example/dakeh/assignment3/ScrapbookModel.java
@@ -1,8 +1,14 @@
 package com.example.dakeh.assignment3;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dakeh on 5/2/2016.
@@ -13,6 +19,26 @@ public class ScrapbookModel extends SQLiteOpenHelper{
 
     private static final String DATABASE_NAME = "ScrapBook";
 
+    private static final String TABLE_COLLECTION = "collection";
+    private static final String TABLE_CLIPPING = "clipping";
+
+    private static final String COLLECTION_ID = "collectionid";
+
+    private static final String KEY_NAME = "name";
+
+    private static final String CLIPPING_ID = "clippingid";
+    private static final String KEY_NOTES = "note";
+    private static final String KEY_DRAWABLEID = "drawableid";
+    private static final String KEY_DATE = "date";
+    private static final String FK_COLLECTION_ID = "fkcollectionid";
+
+    private static final String CREATE_TABLE_COLLECTION = "CREATE TABLE " + TABLE_COLLECTION + "(" + COLLECTION_ID
+            + "PRIMARY KEY," + KEY_NAME + "NAME" + ")";
+
+    private static final String CREATE_TABLE_CLIPPING = "CREATE TABLE " + TABLE_CLIPPING + "(" + CLIPPING_ID
+            + "PRIMARY KEY," + KEY_NOTES + "NOTES," + KEY_DRAWABLEID + "DRAWABLE ID," + KEY_DATE + "DATE," + FK_COLLECTION_ID
+            + "REFERENCES " + TABLE_COLLECTION + "(" + COLLECTION_ID + ")" + ")";
+
 
     public ScrapbookModel(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,23 +46,46 @@ public class ScrapbookModel extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE_COLLECTION);
+        db.execSQL(CREATE_TABLE_CLIPPING);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COLLECTION);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CLIPPING);
 
         onCreate(db);
     }
-    public void createNewCollection() {
+    public void createNewCollection(String n) {
+        SQLiteDatabase db = this.getWritableDatabase();
 
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, n);
+
+        long collection_id = db.insert(TABLE_COLLECTION, null, values);
     }
 
-    public Collection[] getallcollection() {
+    public List<Collection> getallcollection() {
+        List<Collection> collections = new ArrayList<Collection>();
+        String selectQuery = "SELECT * FROM " + TABLE_COLLECTION;
 
-        Collection[] collect = new Collection[100];
+        Log.d("Check:", selectQuery);
 
-        return collect;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Collection collect = new Collection();
+                collect.setID(c.getInt((c.getColumnIndex(COLLECTION_ID))));
+                collect.setName(c.getString(c.getColumnIndex(KEY_NAME)));
+
+                collections.add(collect);
+            }while (c.moveToNext());
+        }
+
+        return collections;
     }
 
     public void createNewClipping() {
